b4d53d8647d0b0ea6a6a10a0151b56ec2c922620
BVFernandes <up200707284@fe.up.pt>
2017-11-21 20:14:47 +0100

    Update Judge
    Update

--- Project/Judge.h
+++ Project/Judge.h
@@ -6,28 +6,76 @@
 
 #include "Calendar.h"
 
-class Judge: public Person {
+class Judge : public Person {
 private:
-    unsigned int id;
-    std::vector<unsigned int> participations;
+	unsigned int id;
+	std::vector<unsigned int> participations;
 public:
-	//Constructors
+	/**
+	* @brief Judge Default Contructor
+	*/
 	Judge() {}
+
+	/**
+	* @brief Judge Contructor with their id, name, address, mobile phone number, date of birth, specialiy and list of participations by id
+	* @param id an unsigined integer, name a string, address a string, mobile an unsigined int, dob a Calendar Object, specialiy a string, participation a vector of unsigined int
+	*/
 	Judge(unsigned int id, std::string name, std::string address, unsigned int mobile, Calendar dob, std::string specialty, std::vector<unsigned int> participation);
+
+	/**
+	* @brief Judge Contructor by reading a line from Judge file
+	* @param textline a string
+	*/
 	Judge(std::string textline);
-	//Get Methods
+
+	/**
+	* @brief Manages to access the Judge's ID value
+	* @return Judge's ID
+	*/
 	unsigned int getId() const;
+
+	/**
+	* @brief Manages to access all participations that the specified Judge has had
+	* @return a constant vector of participations from the specified Judge
+	*/
 	std::vector<unsigned int> getParticipations() const;
-	//Set Methods
+
+	/**
+	* @brief Changes Judge's ID
+	* @param id an unsigined integers
+	*/
 	void setId(unsigned int id);
+
+	/**
+	* @brief Changes the participations the judge was in
+	* @param participation a vector of unsigined integers
+	*/
 	void setParticipations(std::vector<unsigned int> participation);
 
-	//Operator overloading
-	bool operator<(const Judge & judge1) const ;
+	/**
+	* @brief Operator "<" is overloaded to compare the ID value of the Object with a specific Judge
+	* @param judge1 a constant Judge reference
+	* @return true if the Object is greater than judge1 and false if the Object is lesser than judge1
+	*/
+	bool operator<(const Judge & judge1) const;
+
+	/**
+	* @bfief Operator "==" is overloaded to check if the Object Judge is equal to another given Judge in case they share the same properties
+	* @param judge1 a constant Judge reference
+	* @return true if the Object is equal to judge1's properties and false if the Object is not equal to judge1's properties
+	*/
 	bool operator==(const Judge & judge1) const;
+
+	/**
+	* @bfief Operator "<<" is overloaded to output the information about the Judge into a file
+	* @param judge a constant Judge reference
+	* @return ostream reference
+	*/
 	friend std::ostream& operator<<(std::ostream& os, const Judge& judge);
 
-	//Other Methods
+	/**
+	* @brief Prints the Judge's full information on the screen
+	*/
 	void show();
 };
 
